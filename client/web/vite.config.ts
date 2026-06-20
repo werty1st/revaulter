f585b2d0e83eca651f9231968c0f96e6491d8e43
werty1st <werty1st@gmail.com>
2026-06-20 23:39:12 +0200

    fix pwa

--- client/web/vite.config.ts
+++ client/web/vite.config.ts
@@ -54,19 +54,22 @@ export default defineConfig(({ mode }) => {
                     display: 'standalone',
                     icons: [
                         {
-                            src: 'favicon-dark.svg',
-                            sizes: 'any',
-                            type: 'image/svg+xml',
-                        },
-                        {
-                            src: 'icon-192.png',
+                            src: '/icon-192.png',
                             sizes: '192x192',
                             type: 'image/png',
+                            purpose: 'any',
                         },
                         {
-                            src: 'icon-512.png',
+                            src: '/icon-512.png',
                             sizes: '512x512',
                             type: 'image/png',
+                            purpose: 'any',
+                        },
+                        {
+                            src: '/icon-512.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'maskable',
                         },
                     ],
                 },
